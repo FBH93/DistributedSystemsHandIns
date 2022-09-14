ccfe4c88dbce216abe88d9d597ad1335890569c6
Frederik Bøye Henriksen <Fhen@itu.dk>
2022-09-14 13:53:35 +0200

    Working fork putdowns
    Co-authored-by: Kristian Moltke Reitzel <kmoltke@users.noreply.github.com>
    Co-authored-by: jobrha <jobrha@users.noreply.github.com>

--- HandIn1/HandIn1.go
+++ HandIn1/HandIn1.go
@@ -6,50 +6,60 @@ import (
 )
 
 // Each channel corresponds to each fork
-var ch1 = make(chan int, 1)
-var ch2 = make(chan int, 1)
-var ch3 = make(chan int, 1)
-var ch4 = make(chan int, 1)
-var ch5 = make(chan int, 1)
+var ch1Available = make(chan int)
+var ch1PutDown = make(chan int)
+var ch2Available = make(chan int)
+var ch2PutDown = make(chan int)
+var ch3Available = make(chan int)
+var ch3PutDown = make(chan int)
+var ch4Available = make(chan int)
+var ch4PutDown = make(chan int)
+var ch5Available = make(chan int)
+var ch5PutDown = make(chan int)
 
 var maxEats = 3 //Number of times each philospher has to eat.
 
-func philosopher(ID int, fork1 chan int, fork2 chan int) {
+func philosopher(ID int, fork1Avail chan int, fork1PutDown chan int, fork2Avail chan int, fork2PutDown chan int) {
 	ate := 1
 	thoughts := 0
 	fmt.Printf("Philosopher %d is thinking... \n", ID)
 	//loop continues until philosopher eats 3 times
 	for ate <= maxEats {
-		var y = <-fork1 //Receive fork1 when it is available (i.e something in the channel)
-		var x = <-fork2 //receive fork2 when it is available
+		var y = <-fork1Avail //Receive fork1 when it is available (i.e something in the channel)
+		var x = <-fork2Avail //receive fork2 when it is available
 
 		fmt.Printf("Philosopher %d is eating with fork %d and %d for the %d time \n", ID, y, x, ate) //prints when Philosopher eats successfully
 		ate++
 		thoughts++
-		fork1 <- y                                                                         //Put fork1 down
-		fork2 <- x                                                                         //put fork2 down
+		fork1PutDown <- y                                                                  //Put fork1 down
+		fork2PutDown <- x                                                                  //put fork2 down
 		fmt.Printf("Philosopher %d is thinking again with thought %d... \n", ID, thoughts) //Prints when Philosopher is done eating
-
 	}
 	fmt.Printf("Philosopher %d is finished eating \n", ID)
 }
 
-func fork(ID int, ch chan int) {
-	ch <- ID //Fork broadcasts it is available
+func fork(ID int, chAvailable chan int, chPutDown chan int) {
+	chAvailable <- ID //Fork broadcasts it is *initially* available
+	for {             //Checks if the fork has been put down.
+		<-chPutDown
+		fmt.Printf("Fork %d has been put down \n", ID)
+		chAvailable <- ID //If fork has been put down, make it available again
+	}
+
 }
 
 func main() {
 	fmt.Println("Starting Program...")
-	go fork(1, ch1)
-	go fork(2, ch2)
-	go fork(3, ch3)
-	go fork(4, ch4)
-	go fork(5, ch5)
-	go philosopher(1, ch5, ch1)
-	go philosopher(2, ch1, ch2)
-	go philosopher(3, ch2, ch3)
-	go philosopher(4, ch3, ch4)
-	go philosopher(5, ch4, ch5)
+	go fork(1, ch1Available, ch1PutDown)
+	go fork(2, ch2Available, ch2PutDown)
+	go fork(3, ch3Available, ch3PutDown)
+	go fork(4, ch4Available, ch4PutDown)
+	go fork(5, ch5Available, ch5PutDown)
+	go philosopher(2, ch1Available, ch1PutDown, ch2Available, ch2PutDown)
+	go philosopher(1, ch5Available, ch5PutDown, ch1Available, ch1PutDown)
+	go philosopher(3, ch2Available, ch2PutDown, ch3Available, ch3PutDown)
+	go philosopher(4, ch3Available, ch3PutDown, ch4Available, ch4PutDown)
+	go philosopher(5, ch4Available, ch4PutDown, ch5Available, ch5PutDown)
 
 	time.Sleep(5000 * time.Millisecond)
 	fmt.Printf("Finish program")
