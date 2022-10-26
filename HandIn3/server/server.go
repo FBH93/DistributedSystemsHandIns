848a32651283df278f211b51694bf9c48905d577
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2022-10-26 13:04:47 +0200

    Rename mutex locks

--- HandIn3/server/server.go
+++ HandIn3/server/server.go
@@ -16,12 +16,12 @@ import (
 
 type Server struct {
 	pb.UnimplementedChittyChatServer
-	name           string
-	port           string
-	clients        map[string]pb.ChittyChat_ChatServer // Set of clients
-	lampTime       int32
-	mutex_lampTime sync.Mutex
-	mutex_client   sync.Mutex
+	name          string
+	port          string
+	clients       map[string]pb.ChittyChat_ChatServer // Set of clients
+	lampTime      int32
+	mutexLampTime sync.Mutex
+	mutexClient   sync.Mutex
 }
 
 // Flags:
@@ -76,17 +76,17 @@ func launchServer() {
 
 // TODO: Implement error if client name already exists
 func (s *Server) addClient(clientName string, server pb.ChittyChat_ChatServer, cliTime int32) {
-	s.mutex_client.Lock()
+	s.mutexClient.Lock()
 	s.clients[clientName] = server
-	s.mutex_client.Unlock()
+	s.mutexClient.Unlock()
 	s.increaseLamptime(cliTime) //Increase lamptime
 }
 
 // Helper to remove clients
 func (s *Server) removeClient(clientName string, cliTime int32) {
-	s.mutex_client.Lock()
+	s.mutexClient.Lock()
 	delete(s.clients, clientName)
-	s.mutex_client.Unlock()
+	s.mutexClient.Unlock()
 	s.increaseLamptime(cliTime) //increase lamptime
 	leaveMsg := fmt.Sprintf("Participant %s left the server at server Lamport time %d \n", clientName, s.lampTime)
 	log.Printf("[T:%d] %s", s.lampTime, leaveMsg) //log that client has left server.
@@ -95,8 +95,8 @@ func (s *Server) removeClient(clientName string, cliTime int32) {
 
 // Helper to evaluate lamport time
 func (s *Server) increaseLamptime(receivedTime int32) {
-	s.mutex_lampTime.Lock()
-	defer s.mutex_lampTime.Unlock()
+	s.mutexLampTime.Lock()
+	defer s.mutexLampTime.Unlock()
 	if s.lampTime > receivedTime {
 		s.lampTime++
 		fmt.Printf("DEBUG: increased lamptime \n")
