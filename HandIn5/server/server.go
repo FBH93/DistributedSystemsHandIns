88ee1d45f2060262d4fb0cf3930d1711dbe12373
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2022-11-16 21:57:02 +0100

    create server "p2p" architecture

--- HandIn5/server/server.go
+++ HandIn5/server/server.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	auctionPB "github.com/FBH93/DistributedSystemsHandIns/HandIn5/grpc"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
 	"strconv"
 	"sync"
+	"time"
 )
 
 // Flags:
@@ -22,7 +25,8 @@ type Server struct {
 	port         int32
 	leader       bool
 	version      int32
-	nodes        map[int32]auctionPB.AuctionServer
+	ctx          context.Context
+	nodes        map[int32]auctionPB.AuctionClient
 	auctionLive  bool
 	highestBid   int32
 	muHighestBid sync.Mutex
@@ -30,21 +34,29 @@ type Server struct {
 
 func main() {
 	flag.Parse()
-	launchServer()
+	s := launchServer()
+	s.connectToNodes()
 
+	// Keep server alive:
+	for {
+		time.Sleep(time.Second * 5)
+	}
 }
 
-func launchServer() {
+func launchServer() *Server {
 	parsePort, _ := strconv.ParseInt(*port, 10, 32)
 	ownPort := int32(parsePort)
+	ctx, _ := context.WithCancel(context.Background())
+	//defer cancel()
 	server := &Server{
 		name:  *serverName,
 		port:  ownPort,
 		id:    ownPort - 5000,
-		nodes: make(map[int32]auctionPB.AuctionServer),
+		ctx:   ctx,
+		nodes: make(map[int32]auctionPB.AuctionClient),
 	}
 	log.Printf("Attemps to create listener on %d", ownPort)
-	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", ownPort))
 	if err != nil {
 		log.Fatalf("Failed to listen on port %d", ownPort)
 	}
@@ -56,24 +68,38 @@ func launchServer() {
 	grpcServer := grpc.NewServer(opts...)
 	auctionPB.RegisterAuctionServer(grpcServer, server)
 
-	// serve incoming requests:
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
-	}
-	//go func() {
-	//	// Serve incoming requests:
-	//	if err := grpcServer.Serve(lis); err != nil {
-	//		log.Fatalf("Failed to serve: %v", err)
-	//	}
-	//}()
+	go func() {
+		// Serve incoming requests:
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("Failed to serve: %v", err)
+		}
+	}()
+	log.Printf("Server is listening on port %d", ownPort)
+	return server
 }
 
 func (s *Server) connectToNodes() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
 	for i := 0; i < 3; i++ {
 		port := int32(5000) + int32(i)
 		if port == s.port {
 			continue
 		}
+
+		// maybe timeout is useful:
+		//timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
+		//defer cancel()
+
+		log.Printf("Trying to dial #%d", port)
+		conn, err := grpc.Dial(fmt.Sprintf(":%d", port), opts...)
+		if err != nil {
+			log.Fatalf("Failed to dial on port: %d", port)
+		}
+		node := auctionPB.NewAuctionClient(conn)
+		s.nodes[port] = node
+		log.Printf("Successfully dialed and saved node: %d", port)
 
 	}
 }
