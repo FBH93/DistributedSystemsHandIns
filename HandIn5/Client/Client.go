0f78532fb5db9c09fcdccfd113f3cc298db03ec0
Frederik Bøye Henriksen <Fhen@itu.dk>
2022-11-16 15:30:41 +0100

    hopefully working template

---
+++ HandIn5/Client/Client.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	auction "github.com/FBH93/DistributedSystemsHandIns/HandIn5/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// Same principle as in client. Flags allows for user specific arguments/values
+var clientsName = flag.String("name", "default", "Senders name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var client auction.AuctionClient //the client
+var ServerConn *grpc.ClientConn  //the server connection
+
+func main() {
+	//parse flag/arguments
+	flag.Parse()
+
+	fmt.Println("--- CLIENT APP ---")
+
+	//log to file instead of console
+	//f := setLog()
+	//defer f.Close()
+
+	//connect to server and close the connection when program closes
+	fmt.Println("--- join Server ---")
+	ConnectToServer()
+	defer ServerConn.Close()
+
+	//start the input
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		//Do something when pressing enter in console
+	}
+}
+
+// connect to server
+func ConnectToServer() {
+
+	//dial options
+	//the server is not using TLS, so we use insecure credentials
+	//(should be fine for local testing but not in the real world)
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	//dial the server, with the flag "server", to get a connection to it
+	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
+	conn, err := grpc.Dial(fmt.Sprintf(":%s", *serverPort), opts...)
+	if err != nil {
+		log.Printf("Fail to Dial : %v", err)
+		return
+	}
+
+	// makes a client from the server connection and saves the connection
+	// and prints rather or not the connection was is READY
+	client = auction.NewAuctionClient(conn)
+	ServerConn = conn
+	log.Println("the connection is: ", conn.GetState().String())
+}
+
+func bid(bid int32) {
+}
+
+// Function which returns a true boolean if the connection to the server is ready, and false if it's not.
+func conReady(s auction.AuctionClient) bool {
+	return ServerConn.GetState().String() == "READY"
+}
+
+// sets the logger to use a log.txt file instead of the console
+func setLog() *os.File {
+	f, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	return f
+}
---
+++ HandIn5/Server/Server.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+
+	auction "github.com/FBH93/DistributedSystemsHandIns/HandIn5/grpc"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	arg1, _ := strconv.ParseInt(os.Args[1], 10, 32)
+	ownPort := int32(arg1) + 5000
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	server := &server{
+		id:      ownPort,
+		clients: make(map[int32]auction.AuctionClient),
+		ctx:     ctx,
+	}
+
+	// Create listener tcp on port ownPort
+	list, err := net.Listen("tcp", fmt.Sprintf(":%v", ownPort))
+	if err != nil {
+		log.Fatalf("Failed to listen on port: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	auction.RegisterAuctionServer(grpcServer, server)
+
+	go func() {
+		if err := grpcServer.Serve(list); err != nil {
+			log.Fatalf("failed to server %v", err)
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		port := int32(5000) + int32(i)
+
+		if port == ownPort {
+			continue
+		}
+
+		var conn *grpc.ClientConn
+		fmt.Printf("Trying to dial: %v\n", port)
+		conn, err := grpc.Dial(fmt.Sprintf(":%v", port), grpc.WithInsecure(), grpc.WithBlock())
+		if err != nil {
+			log.Fatalf("Could not connect: %s", err)
+		}
+		defer conn.Close()
+		c := auction.NewAuctionClient(conn)
+		server.clients[port] = c
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		//Do something when pressing enter in console
+	}
+}
+
+type server struct {
+	auction.UnimplementedAuctionServer
+	id      int32
+	clients map[int32]auction.AuctionClient
+	ctx     context.Context
+}
+
+func (s *server) bid(ctx context.Context) (auction.reply, error) {
+
+}
+
+func (s *server) result() {
+
+}
